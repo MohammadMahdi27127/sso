93819186deb1e8ed1adbc4e680a11ad4449d03c8
MOHAMMAD27127@ABOZAR <mohammadmahdiabrodi@gmail.com>
2024-12-30 12:05:05 +0330

    conected detabase ✨file env conect to liara

--- server/sso/sso/settings.py
+++ server/sso/sso/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'sso',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'sso.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sso',
+        'USER': 'root',
+        'PASSWORD': 'jcAetE0zOzd9grEdw5UFgwWK',
+        'HOST': 'kazbek.liara.cloud',
+        'PORT': '33786',
     }
 }
 
