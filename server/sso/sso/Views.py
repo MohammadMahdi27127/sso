82c77872bc04db4ac820b4d173885066097477cc
MOHAMMAD27127@ABOZAR <mohammadmahdiabrodi@gmail.com>
2025-01-03 17:15:16 +0330

    ✨tets message

--- server/sso/sso/Views.py
+++ server/sso/sso/Views.py
@@ -4,17 +4,18 @@ from rest_framework import status
 from .models import SMS
 from .Serializer import SMSSerializer
 import requests  # برای ارسال پیامک با کاوه نگار
+from kavenegar import *
 
 @api_view(['GET'])
 def create_sms(request):
-    mobile = request.query_params.get('mobile')  # دریافت شماره موبایل از پارامترهای URL
-    source = request.query_params.get('source')  # دریافت مبدا از پارامترهای URL
+    mobile = request.query_params.get('mobile')
+    source = request.query_params.get('source')
 
     if mobile and source:
         sms_instance = SMS(mobile=mobile, source=source)
-        sms_instance.save()  # ذخیره در پایگاه داده
+        sms_instance.save()
 
-        # ارسال پیامک با کاوه نگار
+
         send_sms(sms_instance.mobile, sms_instance.message)
 
         serializer = SMSSerializer(sms_instance)
@@ -32,6 +33,17 @@ def send_sms(mobile, message):
     }
     response = requests.post(url, data=data)
     return response.json()
+    api = KavenegarAPI('474D71646F756D4E6961784A4374742F4B4A63385142547546577246633179327A6254476576432B6A74553D')
+    params = { 'sender' : '2000660110', 'receptor': '09109063654', 'message' :'.وب سرویس پیام کوتاه کاوه نگار' }
+    response = api.sms_send(params)
+
+    api = KavenegarAPI('Your API Key')
+    params = {
+        'sender': '1000xxxx',
+        'receptor' : '0919xxxxxx',
+        'message' : "Hello Python!"
+    }
+    response = api.sms_send(params)
 
 @api_view(['POST'])
 def verify_message(request):
@@ -43,7 +55,7 @@ def verify_message(request):
         sms_instance = SMS.objects.get(mobile=mobile)
 
         if sms_instance.message == message:
-            # اگر توکن معتبر بود، کاربر می‌تواند به سایت پذیرنده هدایت شود
+
             return Response({"message": "مقدار وارد شده صحیح می باشد،شما می توانید وارد سایت شوید"}, status=status.HTTP_200_OK)
         else:
             return Response({"error": "مقدار وارد شده معتبر نمی باشد"}, status=status.HTTP_400_BAD_REQUEST)
