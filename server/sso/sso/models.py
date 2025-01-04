a180eee86c73175e6a78ecbe404fae199832e286
MOHAMMAD27127@ABOZAR <mohammadmahdiabrodi@gmail.com>
2025-01-04 18:46:11 +0330

    ✨models Auth

--- server/sso/sso/models.py
+++ server/sso/sso/models.py
@@ -25,3 +25,18 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateTimeField(auto_now_add=True)
     source=models.ForeignKey(SMS,on_delete=models.CASCADE,related_name='users')
+
+
+class Auth(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateTimeField(auto_now_add=True)
+    password=models.CharField(max_length=128,null=False,blank=False)
+    last_login = models.DateTimeField(auto_now=True)
+    is_superuser = models.BooleanField(default=False)
+    username = models.CharField(max_length=150,null=False,blank=False,unique=True)
+    last_name = models.CharField(max_length=150,null=False,blank=False)
+    email = models.CharField(max_length=254,null=False,blank=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=150,null=False,blank=False)
