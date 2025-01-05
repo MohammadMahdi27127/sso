838c353125f3d7dabc064b5a93042c4b54df9b03
MOHAMMAD27127@ABOZAR <mohammadmahdiabrodi@gmail.com>
2025-01-05 22:57:13 +0330

    ✨creat models and creat migrations

--- server/sso/sso/models.py
+++ server/sso/sso/models.py
@@ -2,6 +2,7 @@ import random
 import string
 from django.db import models
 
+
 class SMS(models.Model):
     id = models.AutoField(primary_key=True)
     mobile = models.CharField(max_length=11)
@@ -9,7 +10,6 @@ class SMS(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     message = models.TextField()  # نیازی به max_length در TextField نیست
     token = models.CharField(max_length=5)
-    
 
     def save(self, *args, **kwargs):
         # تولید یک توکن تصادفی
@@ -18,13 +18,14 @@ class SMS(models.Model):
 
 
 class Token(models.Model):
-     id = models.AutoField(primary_key=True)
-     token = models.ForeignKey(SMS, on_delete=models.CASCADE,verbose_name='پیام',related_name='Tokens')
-     date = models.DateTimeField(auto_now_add=True)
-     mobile = models.ForeignKey(SMS, on_delete=models.CASCADE,verbose_name='موبایل',related_name='mobiles')
-     source = models.ForeignKey(SMS,on_delete=models.CASCADE,related_name='sources')
+    id = models.AutoField(primary_key=True)
+    token = models.ForeignKey(SMS, on_delete=models.CASCADE, verbose_name='پیام', related_name='Tokens')
+    date = models.DateTimeField(auto_now_add=True)
+    mobile = models.ForeignKey(SMS, on_delete=models.CASCADE, verbose_name='موبایل', related_name='mobiles')
+    source = models.ForeignKey(SMS, on_delete=models.CASCADE, related_name='sources')
+
 
 class User(models.Model):
-     id = models.AutoField(primary_key=True)
-     date = models.DateTimeField(auto_now_add=True)
-     source = models.ForeignKey(SMS, on_delete=models.CASCADE,verbose_name='مبدا',related_name='users')
+    id = models.AutoField(primary_key=True)
+    date = models.DateTimeField(auto_now_add=True)
+    source = models.ForeignKey(SMS, on_delete=models.CASCADE, verbose_name='مبدا', related_name='users')
